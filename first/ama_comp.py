d824d109fa60d61f9ccfe12dc41f1f83d6bf1fef
Nikhil Garg <nikhilgarg2@gmail.com>
2014-07-22 19:09:49 +0530

    latest

---
+++ first/ama_comp.py
@@ -0,0 +1,128 @@
+from selenium import webdriver
+import MySQLdb
+import time
+from .connect import *
+
+def get1(r,prod,proda):
+    try:
+        j=r.find_element_by_css_selector(prod).get_attribute(proda)
+        return j
+    except Exception as d:
+        return "N/A"
+    
+def get2(r,attri):
+    try:
+        j=r.find_element_by_css_selector(attri).text
+        return j
+    except Exception as d:
+        return 0
+
+
+def compile3(css,prod, proda, naam, maxrp, sellp,driver,crawl,category):
+    sql="SELECT * FROM `final` WHERE `crawl_id`=%d" % (int(crawl))
+    cursor.execute(sql)
+    trying=cursor.fetchone()
+    parent=[]
+    links=[]
+    parent=driver.find_elements_by_css_selector(css)
+    sql3="SELECT * FROM `crawl_url` WHERE `id`= %s"
+    valus=trying[7]
+    cursor.execute(sql3,valus)
+    try2=cursor.fetchone()
+    for e in parent:
+        try:
+             x=e.find_element_by_css_selector('div.seeMoreVariations')
+             y=x.find_element_by_css_selector('a')
+             links.append(y)
+        except Exception as d:
+            p=get1(e,prod,proda)
+            n=get2(e,naam)
+            s=get2(e,sellp)
+            m=get2(e,maxrp)
+            if m==0:
+               m=s
+            sql1="INSERT INTO `dump_val`(`crawl_item_id`,`name`,`mrp`,`sp`,`final_id`,`crawl_date`,`reconciled`,\
+                 `reconciled_date`) VALUES(%s,%s,%s,%s,%s,%s,FALSE,%s)"
+            values=(p,n,m,s,try2[1],time.strftime('%Y-%m-%d %H:%M:%S'),time.strftime('%Y-%m-%d %H:%M:%S'))
+            cursor.execute(sql1,values)
+            db.commit()
+    
+    driver1=webdriver.Firefox()
+    for z in links:
+        lk=z.get_attribute('href')
+        driver1.get(lk)
+        e=driver1.find_elements_by_xpath('//*[@data-feature-name="twister"]')
+        #print len(e)
+        x=e[0]
+        e=x.find_elements_by_css_selector('ul.a-button-list')
+
+
+        if len(e)==2:
+            q=e[0].find_elements_by_css_selector('button.a-button-text')
+            for z in q:
+                 z.click()
+                 time.sleep(5)
+                 i=e[1]
+                 p="N/A"
+                 n=get2(driver1,span.a-size-large)
+                 s=get2(driver1,span.a-color-price)
+                 m=get2(driver1,td.a-text-strike)
+                 if m==0:
+                       m=s
+                 sql1="INSERT INTO `dump_val`(`crawl_item_id`,`name`,`mrp`,`sp`,`final_id`,`crawl_date`,`reconciled`,\
+                         `reconciled_date`) VALUES(%s,%s,%s,%s,%s,%s,FALSE,%s)"
+                 values=(p,n,m,s,try2[1],time.strftime('%Y-%m-%d %H:%M:%S'),time.strftime('%Y-%m-%d %H:%M:%S'))
+                 cursor.execute(sql1,values)
+                 db.commit()
+                 r=[]
+                 r=i.find_elements_by_css_selector('li.swatchAvailable')
+                 for g in r:
+                    g.find_element_by_css_selector('button.a-button-text').click()
+                    time.sleep(5)
+                    p="N/A"
+                    n=get2(driver1,span.a-size-large)
+                    s=get2(driver1,span.a-color-price)
+                    m=get2(driver1,td.a-text-strike)
+                    if m==0:
+                       m=s
+                    sql1="INSERT INTO `dump_val`(`crawl_item_id`,`name`,`mrp`,`sp`,`final_id`,`crawl_date`,`reconciled`,\
+                         `reconciled_date`) VALUES(%s,%s,%s,%s,%s,%s,FALSE,%s)"
+                    values=(p,n,m,s,try2[1],time.strftime('%Y-%m-%d %H:%M:%S'),time.strftime('%Y-%m-%d %H:%M:%S'))
+                    cursor.execute(sql1,values)
+                    db.commit()
+                               
+                    #print driver1.find_element_by_css_selector('span.a-size-large').text
+
+        elif len(e)==1:
+  
+              p="N/A"
+              n=get2(driver1,span.a-size-large)
+              s=get2(driver1,span.a-color-price)
+              m=get2(driver1,td.a-text-strike)
+              if m==0:
+                       m=s
+              sql1="INSERT INTO `dump_val`(`crawl_item_id`,`name`,`mrp`,`sp`,`final_id`,`crawl_date`,`reconciled`,\
+                         `reconciled_date`) VALUES(%s,%s,%s,%s,%s,%s,FALSE,%s)"
+              values=(p,n,m,s,try2[1],time.strftime('%Y-%m-%d %H:%M:%S'),time.strftime('%Y-%m-%d %H:%M:%S'))
+              cursor.execute(sql1,values)
+              db.commit()
+              r=[]
+              i=e[0]        
+              r=i.find_elements_by_css_selector('li.swatchAvailable')
+              for g in r:
+                g.find_element_by_css_selector('button.a-button-text').click()
+                time.sleep(5)
+                p="N/A"
+                n=get2(driver1,span.a-size-large)
+                s=get2(driver1,span.a-color-price)
+                m=get2(driver1,td.a-text-strike)
+                if m==0:
+                    m=s
+                sql1="INSERT INTO `dump_val`(`crawl_item_id`,`name`,`mrp`,`sp`,`final_id`,`crawl_date`,`reconciled`,\
+                        `reconciled_date`) VALUES(%s,%s,%s,%s,%s,%s,FALSE,%s)"
+                values=(p,n,m,s,try2[1],time.strftime('%Y-%m-%d %H:%M:%S'),time.strftime('%Y-%m-%d %H:%M:%S'))
+                cursor.execute(sql1,values)
+                db.commit()            
+    driver1.close()    
+
+
