25ec2e46c59aa860e5a43b2429e153b2f72e8d73
Nikhil Garg <nikhilgarg2@gmail.com>
2014-08-01 12:56:11 +0530

    template

---
+++ first/connect_template.py
@@ -0,0 +1,11 @@
+
+'''
+create a file named connect.py in the first folder. The template is given below... Change user,password and database
+details accordingly
+'''
+
+'''import MySQLdb
+
+db=MySQLdb.connect("localhost","USER","PASSWORD","DATABASE")
+cursor=db.cursor()
+'''
---
+++ try_pro/local_settings_template.py
@@ -0,0 +1,21 @@
+
+'''create a file with name as local_settings.py in the same folder as settings.py and fill the details
+in the following format'''
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': '',
+    	'USER': '',
+	'PASSWORD': '',
+ 	'OPTIONS':{
+		'autocommit':True,
+          }
+	}
+}
+
+'''
+
+
+'''also configure the settings in first.connect.py to complete the settings'''
